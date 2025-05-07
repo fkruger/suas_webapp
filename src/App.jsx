2fe771180adbe0a5ffc0d09b289f7149c329b7ac
fkruger <fritz_269@yahoo.com>
2025-05-07 16:06:55 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,3 @@ import SUASUploader from './components/SUASUploader';
 export default function App() {
   return <SUASUploader />;
 }
-
-// src/index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/globals.css';
-
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
